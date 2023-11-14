9280c29188f6c91fed99ba48a5f80cbb4b5bbd19
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2023-11-14 21:36:51 +0200

    update

--- app.py
+++ app.py
@@ -1,4 +1,36 @@
 import streamlit as st
+import seaborn as sns
 
+import utils as ut
+
+
+# App start
 st.title("EMA Browzer 🙃")
-st.header("placeholder to add the app")
+
+with st.sidebar:
+    st.header("Download dataset for editing")
+
+    file_data = ut.read_xls_file()
+    st.download_button(
+        label='Download EMA data', 
+        data=file_data, 
+        file_name='ema_data.xlsx', 
+        key='download_button')
+
+    st.header("Upload updated dataset")
+
+    uploaded_file = st.file_uploader("", type="xlsx")
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        ut.save_xls_file(bytes_data)
+
+data = ut.read_data_from_xls()
+
+dates = []
+for row in data:
+    dates.append(
+        st.selectbox(row[0], row[1]["dates"])
+    )
+
+table = ut.get_table(data, dates)
+st.dataframe(table)
---
+++ utils.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+
+XLS_FILE = "ema_data.xlsx"
+
+
+def parse_dataset(df):
+    key_col = df.columns[0]
+    tmp = df.dropna().astype(str).set_index(key_col)
+
+    dates = list(tmp.index)
+    steps = list(tmp.columns)
+    data = list(tmp.to_records())
+    return {"dates": dates, "steps": steps, "data": data}
+
+
+def read_data_from_xls():
+    meta = pd.read_excel(XLS_FILE, sheet_name="meta")
+    
+    sheets = list(meta.sheet)
+    dataset_names = list(meta.dataset)
+
+    data = []
+    for s, n in zip(sheets, dataset_names):
+        df = pd.read_excel(XLS_FILE, sheet_name=s)
+        data.append((n, parse_dataset(df)))
+    return data
+
+
+def get_table(data, dates):
+    final = []
+    for date, row in zip(dates, data):
+        idx = row[1]["dates"].index(date)
+        for d, st in zip(row[1]["data"][idx], row[1]["steps"]):
+            rec = (d, row[0], st)
+            final.append(rec)
+    df = pd.DataFrame(final)
+    df.columns = ["date", "process", "step"]
+    return df.set_index("date").sort_index()
+
+
+def save_xls_file(bytes_data):
+    with open(XLS_FILE, 'wb') as file:
+        file.write(bytes_data)
+
+
+def read_xls_file():
+    with open(XLS_FILE, 'rb') as file:
+        file_data = file.read()
+    return file_data
+
+
+if __name__ == "__main__":
+    df = read_xls_file()
